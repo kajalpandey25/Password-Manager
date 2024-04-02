5e1bc244d94b1de89d4e752b7d670b6225aaecf3
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-04-02 22:25:53 +0530

    ok

--- PassVault/src/components/Manager.jsx
+++ PassVault/src/components/Manager.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 
 const Manager = () => {
+  const ref = useRef();
+  const [form, setForm] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setPasswordArray] = useState([]);
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords));
+    }
+  }, []);
+
+  const showPassword = () => {
+    alert("show the password");
+    if (ref.current.src.includes === "icons/eyecross.png") {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/eyecross.png";
+    }
+  };
+
+  const savePassword = () => {
+    setPasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+    console.log(passwordArray);
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
       <div className="mycontainer">
         <h1 className="text-4xl text font-bold text-center">
           <span className="text-green-500">&lt;</span>
@@ -14,42 +45,90 @@ const Manager = () => {
           Your own PassWord Manager
         </p>
 
-        <div className="text-white flex flex-col p-4 text-black gap-8 items-center">
+        <div className="text-black flex flex-col p-4 text-black gap-8 items-center">
           <input
+            value={form.site}
+            onChange={handleChange}
             placeholder="Enter website URL"
             className="rounded-full border border-green-500 w-full p-4 py-1"
             type="text"
-            name=""
+            name="site"
             id=""
           />
           <div className="flex w-full justify-between gap-8">
             <input
+              value={form.username}
+              onChange={handleChange}
               placeholder="Enter Username"
               className="rounded-full border border-green-500 w-full p-4 py-1"
               type="text"
-              name=""
+              name="username"
               id=""
             />
             <div className="relative">
               <input
+                value={form.password}
+                onChange={handleChange}
                 placeholder="Enter Password"
                 className="rounded-full border border-green-500 w-full p-4 py-1"
                 type="text"
-                name=""
+                name="password"
                 id=""
               />
-              <span className="absolute right-0 top-0">
-                <img className="p-1" width={30} src="icons/eye.png" alt="eye" />
+              <span
+                className="absolute right-[3px] top-[4px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  width={26}
+                  src="icons/eye.png"
+                  alt="eye"
+                />
               </span>
             </div>
           </div>
-          <button className="flex justify-center items-center gap-2 text-black bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900">
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center gap-2 text-black bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover"
             ></lord-icon>
             Add Password
           </button>
+        </div>
+
+        <div className="passwords">
+          <h2>Your Passwords</h2>
+          <table className="table-auto w-full rounded-md overflow-hidden">
+            <thead className="bg-green-800 text-white">
+              <tr>
+                <th className="py-2">Song</th>
+                <th className="py-2">Artist</th>
+                <th className="py-2">Year</th>
+              </tr>
+            </thead>
+            <tbody className="bg-green-100">
+              <tr>
+                <td className="text-center w-32">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td className="text-center w-32">Malcolm Lockyer</td>
+                <td className="text-center w-32">1961</td>
+              </tr>
+              <tr>
+                <td className="text-center w-32">Witchy Woman</td>
+                <td className="text-center w-32">The Eagles</td>
+                <td className="text-center w-32">1972</td>
+              </tr>
+              <tr>
+                <td className="text-center w-32">Shining Star</td>
+                <td className="text-center w-32">Earth, Wind, and Fire</td>
+                <td className="text-center w-32">1975</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
